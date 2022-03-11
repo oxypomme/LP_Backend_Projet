c7e43613ee05f3c0c2ab3d5f4b59c7d9a2c31443
oxypomme <oxytom8@gmail.com>
2022-03-11 10:29:53 +0100

    chore(authentification): :truck: Correct routes for auth

--- services/authentification/routes/index.ts
+++ services/authentification/routes/index.ts
@@ -1,17 +1,12 @@
 import { createUser } from "../data/users.ts";
 import { Router } from "../deps.ts";
-import { createJWT, default as authJWT } from "../middlewares/jwt.ts";
+import { createJWT } from "../middlewares/jwt.ts";
 
 const router = new Router();
-router
-	.post("/auth", createJWT)
-	.post("/register", async (ctx) => {
-		const payload = await ctx.request.body({ type: "json" }).value;
-		const user = await createUser(payload);
-		ctx.response.body = user;
-	})
-	.get("/", authJWT, (ctx) => {
-		ctx.response.body = { ...ctx.state.jwt };
-	});
+router.post("/signin", createJWT).post("/signout", async (ctx) => {
+	const payload = await ctx.request.body({ type: "json" }).value;
+	const user = await createUser(payload);
+	ctx.response.body = user;
+});
 
 export default router;
