89d4573c63a885cd9f780e45b7686e1f9776f0e5
oxypomme <oxytom8@gmail.com>
2022-03-15 18:01:38 +0100

    fix(gateway-front): :bug: Fixed body parser bug with proxies
    Still need testing tho

--- services/gateway-front/app.ts
+++ services/gateway-front/app.ts
@@ -9,7 +9,7 @@ import router from "./routes";
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
+// app.use(express.json());
 
 app.use(helmet());
 app.use(apiMiddleware);
@@ -19,13 +19,6 @@ app.use(router);
 
 app.get("*", (req, res) => {
 	res.sendError(StatusCodes.NOT_FOUND, "Cette adresse n'existe pas");
-});
-
-app.use("*", (req, res) => {
-	res.sendError(
-		StatusCodes.METHOD_NOT_ALLOWED,
-		"Cette méthode n'est pas autorisée"
-	);
 });
 
 export = app;
