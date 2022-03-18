3068ff541e8d4d361277413ab7d1c5e2ab1bb7e5
oxypomme <oxytom8@gmail.com>
2022-03-18 09:53:43 +0100

    feat(fabrication): :sparkles: Sorted orders by delivery date

--- services/fabrication/data/commandes.js
+++ services/fabrication/data/commandes.js
@@ -6,4 +6,6 @@ export const getAllOrders = async () =>
   await db
     .from("commande")
     .select("id", "nom", "created_at", "livraison", "status")
+    .orderBy("livraison", "desc")
+    .orderBy("created_at", "desc")
     .then();
